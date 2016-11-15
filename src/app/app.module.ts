e7943b1c2ba1189c074ae01f0453619aafaaf2c6
Dag Erik <dagerikhl@gmail.com>
2016-11-15 10:58:54 +0100

    Added basic routing
    No routes yet implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 import { NavbarComponent } from './navbar/navbar.component';
+// import { CourseComponent } from './student-assistant/course/course.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavbarComponent
+        NavbarComponent//,
+        // CourseComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+// import { CourseComponent } from './student-assistant/course/course.component';
+
+export const appRoutes: Routes = [
+    // { path: 'student-assistant/:course', component: CourseComponent },
+    // TODO: Change from placeholder component AppComponent
+    { path: '', component: AppComponent }
+];
